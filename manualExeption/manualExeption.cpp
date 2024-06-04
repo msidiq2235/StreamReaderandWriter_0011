90090757020e749c52366f951e91bdc7afa7928a
msidiq2235 <muhsidiq2005@gmail.com>
2024-06-04 18:41:12 +0700

    done

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -8,4 +8,14 @@ int main()
 		//throw 0,5; //melemparkan sebuah integer maka
 		cout << "Pertanyaan tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "default Pengecualian dieksekusi" << endl;
+	}
+
+	return 0;
 }
