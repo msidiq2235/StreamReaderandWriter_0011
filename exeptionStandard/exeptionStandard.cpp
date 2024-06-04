fa23420801254861d868be237e93e7bddfe18efd
msidiq2235 <muhsidiq2005@gmail.com>
2024-06-04 18:32:05 +0700

    penanda 1

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -4,3 +4,19 @@
 #include <array>
 //untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main() 
+{
+	cout << "Awal Program" << endl; //penanda 1:......
+	try {
+		array<int, 3> data = { 6, 7, 8 };
+		//pesan array integer 3 elemen
+		cout <<data.at(5)<<endl;
+		//memanggil array ke elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+	}
+}
